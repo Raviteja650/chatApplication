b73809f725bbd821324ee767faefc5fbb614c314
Raviteja650 <ravitejab650@gmail.com>
2024-01-03 21:59:49 +0530

    Modified code

--- chatApplication/src/main/java/com/example/chatApplication/Model/MessageType.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatApplication.Model;
-
-public enum MessageType {
-    CHAT,  // when user send the message
-
-    JOIN, // when user joins the chat
-
-    LEAVE  // when user leaves the chat or session
-}
